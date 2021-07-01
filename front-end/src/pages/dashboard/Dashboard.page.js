829a760410530e22deb30a1f32bb8f713d2fa24e
Stephen Carter <cartch@gmail.com>
2021-07-01 15:09:30 -0300

    Add breadcrumb

--- front-end/src/pages/dashboard/Dashboard.page.js
+++ front-end/src/pages/dashboard/Dashboard.page.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { TicketTable } from '../../components/ticket-table/TicketTable.component';
+import { PageBreadcrumb } from '../../components/breadcrumb/Breadcrumb.component';
 import tickets from '../../assets/data/dummy-tickets.json';
 
 export const Dashboard = () => {
   return (
     <Container>
+      <Row>
+        <Col>
+          <PageBreadcrumb page='Dashboard' />
+        </Col>
+      </Row>
       <Row>
         <Col className='text-center mt-5 mb-2'>
           <Button
