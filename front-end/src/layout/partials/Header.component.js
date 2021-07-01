abc40a9f4420cc6299b98e56a579d7688409cbb8
Stephen Carter <cartch@gmail.com>
2021-07-01 14:44:55 -0300

    add dashboard

--- front-end/src/layout/partials/Header.component.js
+++ front-end/src/layout/partials/Header.component.js
@@ -10,7 +10,7 @@ export const Header = () => {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='ms-auto'>
+        <Nav className='ml-auto'>
           <Nav.Link href='/dashboard'>Dashboard</Nav.Link>
           <Nav.Link href='/tickets'>Tickets</Nav.Link>
           <Nav.Link href='/logout'>Logout</Nav.Link>
