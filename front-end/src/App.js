97a2952166de7f70f410ab09660cd0c9ff73168c
Stephen Carter <cartch@gmail.com>
2021-07-01 14:06:30 -0300

    remove root package json

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard.page';
 import { Login } from './pages/home/Home.page';
 
 function App() {
   return (
     <div className='App'>
       {/* <Login /> */}
-      <DefaultLayout>// Dashboard</DefaultLayout>
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   );
 }
