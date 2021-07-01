28ff25c987cb252f330029f6fc879abfe11cae17
Stephen Carter <cartch@gmail.com>
2021-07-01 13:43:42 -0300

    add layout

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import { DefaultLayout } from './layout/DefaultLayout';
 import { Login } from './pages/home/Home.page';
 
 function App() {
   return (
     <div className='App'>
-      <Login />
+      {/* <Login /> */}
+      <DefaultLayout>// Dashboard</DefaultLayout>
     </div>
   );
 }
